858757d7b9d430f6289d2a7f396499bbe4720e65
Muhammad Munib <muhammad.muneeb@PEOPLE.COM>
2023-01-24 11:47:23 +0500

    push code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <nav>
+    <img alt="Vue logo" src="./assets/logo.png"><br>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> | 
     <router-link to="/Contact-us">Contact us</router-link>
